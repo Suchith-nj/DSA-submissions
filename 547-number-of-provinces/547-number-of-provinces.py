0acacbaee1ff6218ef25fe4bb43684d090e0b5ce
Suchith Natraj Javali <95835263+Suchith-nj@users.noreply.github.com>
2022-09-27 20:28:48 -0500

    Time: 487 ms (18.25%), Space: 14.1 MB (96.55%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.py
+++ 547-number-of-provinces/547-number-of-provinces.py
@@ -3,6 +3,9 @@ class Solution:
         
         def dfs(isConnected,visited,i):
             for j in range(len(isConnected)):
+                #since it is symetric matrix, if we have already visted [i][j]
+                # we don't have to visit [j][i]
+                #adding i and j will take care of this condition
                 if isConnected[i][j] == 1 and j not in visited:
                     visited.append(j)
                     dfs(isConnected,visited,j)
@@ -10,7 +13,9 @@ class Solution:
         visited = []
         count = 0
         for i in range(len(isConnected)):
+            #we append j in visited but check for i also
             if i not in visited:
                 dfs(isConnected,visited,i)
                 count += 1
         return count 
+    
