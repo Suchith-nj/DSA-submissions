261e60cec7e3670aa5d72a0c423710eaea739e78
Suchith Natraj Javali <95835263+Suchith-nj@users.noreply.github.com>
2022-09-30 00:49:24 -0500

    Time: 513 ms (15.58%), Space: 14.2 MB (96.57%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.py
+++ 547-number-of-provinces/547-number-of-provinces.py
@@ -1,21 +1,21 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        
-        def dfs(isConnected,visited,i):
+        visited = []
+        def dfs(isConnected,i):
             for j in range(len(isConnected)):
                 #since it is symetric matrix, if we have already visted [i][j]
                 # we don't have to visit [j][i]
                 #adding i and j will take care of this condition
                 if isConnected[i][j] == 1 and j not in visited:
                     visited.append(j)
-                    dfs(isConnected,visited,j)
+                    dfs(isConnected,j)
         
-        visited = []
+        
         count = 0
         for i in range(len(isConnected)):
             #we append j in visited but check for i also
             if i not in visited:
-                dfs(isConnected,visited,i)
+                dfs(isConnected,i)
                 count += 1
         return count 
     
